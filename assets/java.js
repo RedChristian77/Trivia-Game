70e503f283c83f6575749c22856873f563b6089d
Christian Breyer <Christian.d.breyer@Outlook.com>
2019-08-08 20:18:29 -0500

    Base questions/Java/CSS created

---
+++ assets/java.js
@@ -0,0 +1,56 @@
+const questions = [
+    question1 = {
+        quest : "The armor of Artorias is designed after what Anime?",
+        answers : ["Beserk","Claymore","Overlord","Akame Ga Kill"]
+    },
+    question2 = {
+        quest : "What is the name of the starting Location?",
+        answers : ["Undead Asylum","Undead Parish","Quelaag's Domain", "Firelink Shrine"]
+    },
+    question3 = {
+        quest : "Who is the daughter of Seath the Scaleless?",
+        answers : ["Crossbreed Priscilla","Elizabeth","Quelana of Izalith","Reah of Thorolund"]
+    },
+    question4 = {
+        quest: "Which NPC gives the ring of Favor and Protection (The Fap Ring)",
+        answers : ["Knight Lautrec","Solaire of Astoria","Nico of Thorolund","Siegmeyer of Catarina"]
+    },
+    question5 = {
+        quest : "What is the Name of Artoria's companion?",
+        answers : ["Great Grey Wolf Silf","Domhnall of Zena","Petrus of Thorolund","Darkstalker Kaeethe"]
+    },
+    question6 = {
+        quest : "What is the only weapon that has a cursed version?",
+        answers : ["Greatsword of Artorias","Crystal Greatsword","Dragon Greatsword","Chaos Blade"]
+    },
+    question7 = {
+        quest : "Andre of Astora, Giant Blacksmith, and Vamos and who else is all blacksmiths?",
+        answers : ["Rickert of Vinheim", "Gravelord Nito","Big Hat Logan","Hawkeye Gough"]
+    },
+    question8 = {
+        quest : "During the first playthrough its possible to unlock every Miracle but which one?",
+        answers : ["Sunlight Spear", "Tranquil walk of Peace","Vow of Silence","Darkmoon Blade"]
+    },
+    question9 = {
+        quest : "What covenent's joining location is Ash Lake?",
+        answers : ["Path of the Dragon","Gravelord Servant","Forest Huntr","Chaos Servant"],
+    },
+    question10 = {
+        quest : "Who is the director of Dark Souls?",
+        answers : ["Hidetaka Miyazaki","Hideo Kojima","Shigeru Miyamoto","Shenji Mikami"],
+    }
+
+]
+
+let answered = false;
+let guess = "";
+
+//console.log(questions[1].quest);
+function rungame(){
+for(i=0; i<questions.length-1;i++){
+    
+    while(answered === false){
+
+    }
+}
+};
